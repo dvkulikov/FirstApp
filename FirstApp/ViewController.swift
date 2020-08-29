0b468809877557898e4fbb224678537fc94f1003
dv.kulikov <dv.kulikov@mail.ru>
2020-08-29 20:07:13 +0300

    Initial Commit

---
+++ FirstApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FirstApp
+//
+//  Created by 1 on 29.08.2020.
+//  Copyright © 2020 Denis Kulikov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
